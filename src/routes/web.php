05d7c098ea6592f7c60de0da5621370461a40bf4
onaboka <nabokaoleh@gmail.com>
2025-01-05 23:29:34 +0100

    LA-3 - Breeze Login

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'contact')->name('contact.page');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
